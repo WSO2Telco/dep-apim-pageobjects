0bfb3f4704ad036753071535933612d90092166d
SulakkhanaW <SulakkhanaW@bitbucket.org>
2016-02-02 16:45:07 +0530

    review comments incorparated

--- src/main/java/com/wso2telco/apimanager/pageobjects/apihome/sandbox/SandBoxPage.java
+++ src/main/java/com/wso2telco/apimanager/pageobjects/apihome/sandbox/SandBoxPage.java
@@ -8,28 +8,54 @@ import com.wso2telco.apimanager.pageobjects.apihome.manager.ManagerPage;
 import com.wso2telco.test.framework.core.WebPelement;
 import com.wso2telco.test.framework.util.UIType;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SandBoxPage.
+ */
 public class SandBoxPage extends BasicPageObject  {
 	
+	/** The logger. */
 	Logger logger = Logger.getLogger(ManagerPage.class);
 
+	/** The lbl login. */
 	private WebPelement lblLogin = defineEelement(UIType.Xpath, "//div[@class='title-section']/h2");
 	
+	/** The txt user name. */
 	private WebPelement txtUserName = defineEelement(UIType.Xpath, "//form[@id='loginForm']/input[1]");
 	
+	/** The txt password. */
 	private WebPelement txtPassword = defineEelement(UIType.Xpath, "//form[@id='loginForm']/input[2]");
 	
-	private WebPelement btnLogIn = defineEelement(UIType.Xpath, "//input[@id='loginButton']");
+	/** The btn log in. */
+	private WebPelement btnLogIn = defineEelement(UIType.ID, "loginButton");
 	
+	/** The lbl welcome. */
 	private WebPelement lblWelcome = defineEelement(UIType.Xpath, "//div[@class='title-section']/h2");
 	
-	private WebPelement btnUserName = defineEelement(UIType.Xpath, "//a[@id='userMenu']");
+	/** The btn user name. */
+	private WebPelement btnUserName = defineEelement(UIType.ID, "userMenu");
 	
+	/** The btn logout. */
 	private WebPelement btnLogout = defineEelement(UIType.Xpath, "//li[@class='logout-link-special']/button");
 	
+	/**
+	 * Instantiates a new sand box page.
+	 *
+	 * @author SulakkhanaW
+	 * @param driver the driver
+	 */
 	public SandBoxPage(WebDriver driver) {
 		super(driver);
 	}
 
+	/**
+	 * Checks if is sandbox login page.
+	 *
+	 * @author SulakkhanaW
+	 * @param sandbobLoginHeader the sandbob login header
+	 * @return true, if is sandbox login page
+	 * @throws Exception the exception
+	 */
 	public boolean isSandboxLoginPage(String sandbobLoginHeader) throws Exception{
 		flag = false;
 		logger.debug("Validating Sandbox login page");
@@ -47,18 +73,49 @@ public class SandBoxPage extends BasicPageObject  {
 		return flag;
 	}
 	
+	/**
+	 * Enter user name.
+	 *
+	 * @author SulakkhanaW
+	 * @param username the username
+	 */
 	public void enterUserName(String username){
+		logger.debug("Entering Username");
 		getElement(txtUserName).clearAndSendkeys(username);
+		logger.debug("Entered Username successfully");
 	}
 	
+	/**
+	 * Enter password.
+	 *
+	 * @author SulakkhanaW
+	 * @param password the password
+	 */
 	public void enterPassword(String password){
+		logger.debug("Entering password");
 		getElement(txtPassword).clearAndSendkeys(password);
+		logger.debug("Entered password successfully");
 	}
 	
+	/**
+	 * Click login.
+	 *
+	 * @author SulakkhanaW
+	 */
 	public void clickLogin(){
+		logger.debug("Clicking on login");
 		getElement(btnLogIn).click();
+		logger.debug("Clicked on login");
 	}
 	
+	/**
+	 * Checks if is sandbox welcome.
+	 *
+	 * @author SulakkhanaW
+	 * @param welcome the welcome
+	 * @return true, if is sandbox welcome
+	 * @throws Exception the exception
+	 */
 	public boolean isSandboxWelcome(String welcome) throws Exception{
 		flag = false;
 		logger.debug("Validating Sandbox welcome page");
@@ -76,11 +133,25 @@ public class SandBoxPage extends BasicPageObject  {
 		return flag;
 	}
 	
+	/**
+	 * Click user name.
+	 *
+	 * @author SulakkhanaW
+	 */
 	public void clickUserName(){
+		logger.debug("Clicking on username");
 		getElement(btnUserName).click();
+		logger.debug("Clicked on username");
 	}
 	
+	/**
+	 * Click logout.
+	 *
+	 * @author SulakkhanaW
+	 */
 	public void clickLogout(){
+		logger.debug("Clicked on logout");
 		getElement(btnLogout).click();
+		logger.debug("Clicking on logout");
 	}
 }
