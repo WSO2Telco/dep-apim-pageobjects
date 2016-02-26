1f43bb2157090964342850964d488f89b07f13f5
unknown <AUX-026@SulakkhanaW.SulakkhanaW>
2016-02-26 17:29:31 +0530

    operator api traffic report validations completed

--- src/main/java/com/wso2telco/apimanager/pageobjects/db/queries/SQLQuery.java
+++ src/main/java/com/wso2telco/apimanager/pageobjects/db/queries/SQLQuery.java
@@ -13,4 +13,11 @@ public interface SQLQuery {
 	 * */
 	String TOTAL_API_TRAFFIC = "select sb.api, COUNT(*) as trafficCount from SB_API_RESPONSE_SUMMARY sb where sb.time BETWEEN  '%s' and '%s' and sb.operatorId = '%s' and sb.userId = '%s' GROUP BY sb.api";
 	
+	/** The operator api traffic. 
+	 * %s_1 - fromDate
+	 * %s_2 - toDate
+	 * %s_4 - userId
+	 * */
+	String OPERATOR_API_TRAFFIC = "select sb.operatorId, COUNT(*) as trafficCount from SB_API_RESPONSE_SUMMARY sb where sb.time BETWEEN  '%s' and '%s' and sb.userId = '%s' GROUP BY sb.operatorId";
+	
 }
