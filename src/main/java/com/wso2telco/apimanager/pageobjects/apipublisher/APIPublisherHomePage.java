8de6bb05990da251117050ae048a54b0b86faa0d
Lahiru <malshani.senarathne@auxenta.com>
2017-01-20 12:22:13 +0530

    Modified

--- src/main/java/com/wso2telco/apimanager/pageobjects/apipublisher/APIPublisherHomePage.java
+++ src/main/java/com/wso2telco/apimanager/pageobjects/apipublisher/APIPublisherHomePage.java
@@ -2,7 +2,6 @@ package com.wso2telco.apimanager.pageobjects.apipublisher;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
